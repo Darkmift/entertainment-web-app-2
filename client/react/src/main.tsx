f453780941b4df7442881728e8954e02c2d495fd
darkmift <darkmift@gmail.com>
2023-02-19 12:26:43 +0200

    initial app scaffolding

--- client/react/src/main.tsx
+++ client/react/src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/main.scss'
+import { RouterProvider } from 'react-router-dom'
+import router from './router'
+import { Provider } from 'react-redux'
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <RouterProvider
+                router={router}
+                fallbackElement={<React.Fragment>Loading ...</React.Fragment>}
+            />
+        </Provider>
+    </React.StrictMode>
 )
---
+++ client/react/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SVGIcon from '../components/SVGIcon'
+
+type Props = {}
+
+function Home({}: Props) {
+    return (
+        <div>
+            <SVGIcon iconName="clip" /*svgProp={{ fill: '#ccc' }} */ />
+        </div>
+    )
+}
+
+export default Home
