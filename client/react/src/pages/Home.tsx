711a71c6f577716750c09799c1778bf565c9826d
darkmift <darkmift@gmail.com>
2023-02-19 19:33:49 +0200

    applied iconPanel

--- client/react/src/pages/Home.tsx
+++ client/react/src/pages/Home.tsx
@@ -1,22 +1,32 @@
-import SVGIcon from '../components/SVGIcon'
+import IconPanel from 'components/IconPanel'
 import styled from 'styled-components'
+import { SetStateAction, useState } from 'react'
+import { CategoryName } from 'types'
 
 type Props = {}
 
 const HomeContainer = styled.div`
     display: flex;
-    flex-direction: column;
-    align-items: center;
+    padding: 32px 0 0 32px;
+    gap: 0 36px;
+
+    .media-browser {
+        background-color: var(--dark-blue);
+    }
 `
 
 function Home({}: Props) {
+    const [category, setCategory] = useState<CategoryName>('all-media')
     return (
         <HomeContainer>
-            <SVGIcon
-                iconName="clip"
-                wrapperStyle={{ marginTop: '78px' }}
-                // svgProp={{ fill: '#ccc' }}
+            <IconPanel
+                category={category}
+                onIconClick={(categoryName: string) => {
+                    console.log('icon set', { iconName: categoryName })
+                    setCategory(categoryName as SetStateAction<CategoryName>)
+                }}
             />
+            <div className="media-browser">MEDIA AREA</div>
         </HomeContainer>
     )
 }
