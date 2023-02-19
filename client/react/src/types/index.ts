6cac03fa99e195b762618d644ee04103962e4b28
darkmift <darkmift@gmail.com>
2023-02-19 19:32:33 +0200

    added type

--- client/react/src/types/index.ts
+++ client/react/src/types/index.ts
@@ -3,3 +3,4 @@ export * from './user'
 
 export type BasicAttrObject = { [key: string]: string }
 export type CSSObject = BasicAttrObject
+export type CategoryName = 'all-media' | 'movies' | 'series' | 'favorites'
