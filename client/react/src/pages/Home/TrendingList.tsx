2ceb357f9cfee11ae650f14ac5d34080ac0c1d89
darkmift <darkmift@gmail.com>
2023-04-27 10:57:33 +0300

    applied horizontal drag and scroll

--- client/react/src/pages/Home/TrendingList.tsx
+++ client/react/src/pages/Home/TrendingList.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import useDrag from '@/hooks/useDrag'
+import { useState } from 'react'
 import styled from 'styled-components'
 import { Media } from 'types'
 import TrendingCard from './TrendingCard'
+
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu'
+import 'react-horizontal-scrolling-menu/dist/styles.css'
 
 type Props = { items: Media[] }
 
 const ListContainer = styled.div`
     display: flex;
     flex-direction: column;
+    gap: 25px;
     .trending-media-list {
         padding-top: 25px;
         display: flex;
@@ -15,16 +20,56 @@ const ListContainer = styled.div`
         overflow-x: scroll;
     }
 `
+type scrollVisibilityApiType = React.ContextType<typeof VisibilityContext>
+
+function onWheel(apiObj: scrollVisibilityApiType, ev: React.WheelEvent): void {
+    const isThouchpad = Math.abs(ev.deltaX) !== 0 || Math.abs(ev.deltaY) < 15
+
+    if (isThouchpad) {
+        ev.stopPropagation()
+        return
+    }
+
+    if (ev.deltaY < 0) {
+        apiObj.scrollNext()
+    } else if (ev.deltaY > 0) {
+        apiObj.scrollPrev()
+    }
+}
 
 function TrendingList({ items }: Props) {
+    const { dragStart, dragStop, dragMove, dragging } = useDrag()
+
+    const [selected, setSelected] = useState<string>('')
+
+    const handleItemClick = (itemId: string) => () => {
+        if (dragging) {
+            return false
+        }
+        setSelected(selected !== itemId ? itemId : '')
+    }
+    const handleDrag =
+        ({ scrollContainer }: scrollVisibilityApiType) =>
+        (ev: React.MouseEvent) =>
+            dragMove(ev, (posDiff) => {
+                if (scrollContainer.current) {
+                    scrollContainer.current.scrollLeft += posDiff
+                }
+            })
+
     return (
-        <ListContainer>
+        <ListContainer onMouseLeave={dragStop}>
             <h1>Trending</h1>
-            <div className="trending-media-list">
+            <ScrollMenu
+                onWheel={onWheel}
+                onMouseDown={() => dragStart}
+                onMouseUp={() => dragStop}
+                onMouseMove={handleDrag}
+            >
                 {items.map((item, index) => (
                     <TrendingCard key={index} item={item} />
                 ))}
-            </div>
+            </ScrollMenu>
         </ListContainer>
     )
 }
