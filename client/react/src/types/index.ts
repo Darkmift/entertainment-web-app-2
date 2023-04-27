f7be9a53b2ece0d86f13ab57b1cc31e2a7cdfca2
darkmift <darkmift@gmail.com>
2023-04-27 14:31:13 +0300

    added type

--- client/react/src/types/index.ts
+++ client/react/src/types/index.ts
@@ -1,6 +1,9 @@
+import { Media } from './media'
+
 export * from './media'
 export * from './user'
 
 export type BasicAttrObject = { [key: string]: string }
 export type CSSObject = BasicAttrObject
+export type MediaItemHandler = (item: Media) => void
 export type CategoryName = 'all-media' | 'movies' | 'series' | 'favorites'
